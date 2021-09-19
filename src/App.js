927e7437b6a8654c07a788bb2474946576794a93
IsmaelLebzo <20017635@student.ltuc.com>
2021-09-19 23:26:25 +0300

    finished lab06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      locaResualt: {},
+      searchQuery: '',
+      showLocInfo: false
+    }
+  }
+  getLocInfo = async (e) =>{
+    e.preventDefault();
+    await this.setState({
+      searchQuery: e.target.city.value
+    })
+
+    let reqUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`
+
+    let loctionResault = await axios.get(reqUrl);
+
+    this.setState({
+      locaResualt: loctionResault.data[0],
+      showLocInfo: true
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h3>City Explore app</h3>
+        <form onSubmit={this.getLocInfo}>
+          <input type="text" name='city'/>
+          <input type="submit" value='get city info' />
+        </form>
+
+        {/* {this.state.showLocInfo && 
+        <>
+        <p>City Name: {this.state.searchQuery}</p>
+        <p>Latitude: {this.state.locaResualt.lat}</p>
+        <p>Longitude: {this.state.locaResualt.lon}</p>
+
+        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locaResualt.lat},${this.state.locaResualt.lon}&zoom=12`} alt="city" />
+        </>
+        } */}
+        <>
+        <p>City Name: {this.state.searchQuery}</p>
+        <p>Latitude: {this.state.locaResualt.lat}</p>
+        <p>Longitude: {this.state.locaResualt.lon}</p>
+
+        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locaResualt.lat},${this.state.locaResualt.lon}&zoom=12`} alt="city" />
+        </>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
