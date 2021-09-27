9a706c287aac11c53943c0d584c5f8912994e6c2
IsmaelLebzo <20017635@student.ltuc.com>
2021-09-27 22:56:51 +0300

    testing

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,27 +1,40 @@
-import React, { Component } from "react";
-import Card from 'react-bootstrap/Card'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import { Table } from 'react-bootstrap'
 
-class Movie extends Component {
+class Movie extends React.Component {
     render() {
         return (
-            <div>
-                <Card style={{ width: '100%' }}>
-                    <Card.Body>
-                        <Card.Title>Movie info</Card.Title>
-                        <Card.Text>
-                            <p>Title: {this.props.MovieResualt.title}</p>
-                            <p>Overview: {this.props.MovieResualt.overview}</p>
-                            <p>Average Votes: {this.props.MovieResualt.average_votes}</p>
-                            <p>Total Votes: {this.props.MovieResualt.total_votes}</p>
-                            <img src={this.props.MovieResualt.image_url} alt={this.props.alt}  style={{ width: '100%' }}/>
-                            <p>Popularity: {this.props.MovieResualt.popularity}</p>
-                            <p>Released On: {this.props.MovieResualt.released_on}</p>
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
+            <Table striped bordered hover >
+                <thead>
+                    <tr>
+                        <th>Movie Title</th>
+                        <th>Overview</th>
+                        <th>AverageVotes</th>
+                        <th>TotalVotes</th>
+                        <th>ImageUrl</th>
+                        <th>Popularity</th>
+                        <th>ReleasedOn</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.MovieResult.map((element, idx) => {
+                            return (
+                                <tr key={idx}>
+                                    <td >{element.title}</td>
+                                    <td>{element.overview}</td>
+                                    <td>{element.averageVotes}</td>
+                                    <td>{element.totalVotes}</td>  
+                                    <td><img src={element.imageUrl} alt='img' /></td>
+                                    <td>{element.popularity}</td>
+                                    <td>{element.releasedOn}</td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </Table>
         )
     }
 }
-export default Movie;
+
+export default Movie
